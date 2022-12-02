d43ee5a0fdd6dbeb54738361d36675b85d50a19d
AnshulYADAV <anshulyadav98@gmail.com>
2022-12-02 05:55:09 +0530

    Added Longest Common Substring

---
+++ Dynamic Programming/Longest Common Substring/longest-increasing-subsequence.py
@@ -0,0 +1,27 @@
+
+from functools import lru_cache
+
+
+def findLCSLength(s1, s2):
+    @lru_cache(10000)
+    def helper(i1, i2):  # dp[i1][i2][count] = 4
+        if i1 == len(s1) or i2 == len(s2):
+            return 0
+
+        choice1 = 0
+        if s1[i1] == s2[i2]:
+            choice1 = 1 + helper(i1 + 1, i2 + 1)
+
+        choice2 = helper(i1 + 1, i2)
+        choice3 = helper(i1, i2 + 1)
+
+        return max([choice1, choice2, choice3])
+    return helper(0, 0)
+
+
+def findLongestIncreasingSubsequence(nums):
+    return findLCSLength(nums, sorted(nums))
+
+
+nums = [4, 2, 3, 6, 10, 1, 12]
+print(findLongestIncreasingSubsequence(nums))
